trbom5c/vikunja-custom

commit 8cea5b162358422825dd6eb276a1ca13140f830c

fix(navigation): do not hide shadows of dropdown menu

frontend/src/components/home/navigation.vue
@@ -155,7 +155,6 @@ const savedFilterProjects = computed(() => projectStore.savedFilterProjects)
 	bottom: 0;
 	left: 0;
 	transform: translateX(-100%);
-	overflow-x: auto;
 	width: $navbar-width;
 
 	@media screen and (max-width: $tablet) {

commit fb1f3ba96a7474d75d1f3f5061f0ffb3a3537b61

fix(navigation): scrolling when many projects are present
Regression from 8cea5b162358422825dd6eb276a1ca13140f830c
Resolves https://github.com/go-vikunja/vikunja/issues/249

frontend/src/components/home/navigation.vue
@@ -156,6 +156,7 @@ const savedFilterProjects = computed(() => projectStore.savedFilterProjects)
 	left: 0;
 	transform: translateX(-100%);
 	width: $navbar-width;
+	overflow-y: auto;
 
 	@media screen and (max-width: $tablet) {
 		top: 0;