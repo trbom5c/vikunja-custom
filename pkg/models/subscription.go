trbom5c/vikunja-custom

commit 3655b6a9b3a7c76aadfc4676881bc8667ad6ff74

fix(subscriptions): ignore task subscription when the user is subscribed to the project
Resolves https://community.vikunja.io/t/e-mail-notification-twice/2740/12
Resolves https://github.com/go-vikunja/vikunja/issues/316

pkg/models/subscription.go
@@ -234,23 +234,12 @@ func GetSubscriptions(s *xorm.Session, entityType SubscriptionEntityType, entity
 			return nil, err
 		}
 
-		// If the task does not have a subscription directly or from its project, get the one
-		// from the parent and return it instead.
-		var taskIDsWithoutSubscription []int64
-		for _, eID := range entityIDs {
-			if _, has := subs[eID]; has {
-				continue
-			}
-
-			taskIDsWithoutSubscription = append(taskIDsWithoutSubscription, eID)
-		}
-
-		projects, err := GetProjectsSimplByTaskIDs(s, taskIDsWithoutSubscription)
+		projects, err := GetProjectsSimplByTaskIDs(s, entityIDs)
 		if err != nil {
 			return nil, err
 		}
 
-		tasks, err := GetTasksSimpleByIDs(s, taskIDsWithoutSubscription)
+		tasks, err := GetTasksSimpleByIDs(s, entityIDs)
 		if err != nil {
 			return nil, err
 		}
@@ -261,9 +250,17 @@ func GetSubscriptions(s *xorm.Session, entityType SubscriptionEntityType, entity
 		}
 
 		for _, task := range tasks {
-			sub, has := projectSubscriptions[task.ProjectID]
-			if has {
-				subs[task.ID] = sub
+			// If a task is already subscribed through the parent project,
+			// remove the task subscription since that's a duplicate.
+			// But if the user is not subscribed to the task but a parent project is, add that to the subscriptions
+			psub, hasProjectSub := projectSubscriptions[task.ProjectID]
+			_, hasTaskSub := subs[task.ID]
+			if hasProjectSub && hasTaskSub {
+				delete(subs, task.ID)
+			}
+
+			if !hasTaskSub {
+				subs[task.ID] = psub
 			}
 		}
 

commit 339d0501675e5c49372000240f94e1e544642644

fix(notifications): only add project subscription as task subscription when the user is not already subscribed to the task
Before this fix, a project subscription object was added twice to the list of subscriptions for a task when the user did not subscribe to the task directly. This caused the user to receive a comment notification twice for a given task.
This was probably a regression from 3655b6a9b3.

Resolves https://community.vikunja.io/t/e-mail-notification-twice/2740/18

pkg/models/subscription.go
@@ -259,7 +259,7 @@ func GetSubscriptions(s *xorm.Session, entityType SubscriptionEntityType, entity
 				delete(subs, task.ID)
 			}
 
-			if !hasTaskSub {
+			if !hasTaskSub && !hasProjectSub {
 				subs[task.ID] = psub
 			}
 		}