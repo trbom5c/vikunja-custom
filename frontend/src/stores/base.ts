trbom5c/vikunja-custom

commit 08a7747bc0182e258dc24fb3c28cfce1a0318f85

fix(task): set project after loading task details
Resolves https://github.com/go-vikunja/vikunja/issues/1342

frontend/src/views/tasks/TaskDetailView.vue
@@ -743,6 +743,10 @@ watch(
 			attachmentStore.set(task.value.attachments)
 			taskColor.value = task.value.hexColor
 			setActiveFields()
+
+			if (project.value) {
+				await baseStore.handleSetCurrentProject({project: project.value})
+			}
 		} catch (e) {
 			if (e?.response?.status === 404) {
 				router.replace({name: 'not-found'})

commit 0a3a2edc870caec39ba0449835034f7f4011c638

fix(kanban): update task on board when changing details
Fixes a regression introduced in 08a7747bc0182e258dc24fb3c28cfce1a0318f85

frontend/src/stores/base.ts
@@ -137,6 +137,12 @@ export const useBaseStore = defineStore('base', () => {
 		setCurrentProject(project, currentProjectViewId)
 	}
 
+	async function handleSetCurrentProjectIfNotSet(project: IProject) {
+		if (currentProject.value?.id !== project.id) {
+			await handleSetCurrentProject({project})
+		}
+	}
+
 	async function loadApp() {
 		try {
 			await checkAndSetApiUrl(window.API_URL)
@@ -184,6 +190,7 @@ export const useBaseStore = defineStore('base', () => {
 		setUpdateAvailable,
 
 		handleSetCurrentProject,
+		handleSetCurrentProjectIfNotSet,
 
 		...useMenuActive(),
 	}

frontend/src/views/tasks/TaskDetailView.vue
@@ -745,7 +745,7 @@ watch(
 			setActiveFields()
 
 			if (project.value) {
-				await baseStore.handleSetCurrentProject({project: project.value})
+				await baseStore.handleSetCurrentProjectIfNotSet(project.value)
 			}
 		} catch (e) {
 			if (e?.response?.status === 404) {